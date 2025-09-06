0fdd7201ae6f88155f3072751b73b1ad0caba082
ishaandesai <IshaanDesai21@gmail.com>
2025-09-06 16:59:53 -0500

    Decode Day 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/subsystems/VariableHood.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/subsystems/VariableHood.java
@@ -2,26 +2,25 @@ package org.firstinspires.ftc.teamcode.nextFTC.subsystems;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.ServoEx;
 import dev.nextftc.hardware.positionable.SetPosition;
 
-public class ShooterAngle implements Subsystem {
-    public static final ShooterAngle INSTANCE = new ShooterAngle();
-    private ShooterAngle() { }
+public class VariableHood implements Subsystem {
+    public static final VariableHood INSTANCE = new VariableHood();
+    private VariableHood() { }
 
-    private final ServoEx shooterAngle = new ServoEx("shooterAngle");
+    private final ServoEx variableHood = new ServoEx("variableHood");
 
     //Calculation code here
 
     public Command up = new SetPosition(
-            shooterAngle,
+            variableHood,
             0.1
     ).requires(this);
 
 
     public Command down = new SetPosition(
-            shooterAngle,
+            variableHood,
             0.2
     ).requires(this);
 }
@@ -29,7 +28,7 @@ public class ShooterAngle implements Subsystem {
 //Range options
 /*
 April Tags - For turret angle, to get distance for Shooter Angle
-Limelight -
+Limelight
 Ultrasonic
 
  */
