cb3df220de998d7652f11584603466900ea0b7da
Aubrey Tipps <aatipps@gmail.com>
2025-09-06 22:50:32 -0500

    adding boilerplate jawns

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/intake/Intake.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.mechanisms.intake;
+
+import dev.nextftc.core.subsystems.Subsystem;
+
+public class Intake implements Subsystem {
+    public static final Intake INSTANCE = new Intake();
+    private Intake() { }
+
+    // Hardware
+
+
+}
