bdda0bc750665071e12ba9592618168f869de77b
Aubrey Tipps <aatipps@gmail.com>
2025-09-15 07:49:08 -0500

    updating shooter mech

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/subsystems/shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nextFTC/subsystems/shooter/Shooter.java
@@ -12,106 +12,96 @@ import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.impl.ServoEx;
 
 public class Shooter implements Subsystem {
-//    public static final Shooter INSTANCE = new Shooter();
-//    private Shooter() { }
+    // NextFTC Shooter Instance
+    public static final Shooter INSTANCE = new Shooter();
+    private Shooter() { }
 
-//    private MotorEx outtakeL; // 0
-//    private MotorEx outtakeR;
+    // NextFTC Motors
+    private final MotorEx autoOuttakeL = new MotorEx("outtakeLeft"); // 0 CH
+    private final MotorEx autoOuttakeR = new MotorEx("outtakeRight"); // 2 CH
 
-    public DcMotorEx outtakeL; // 0
-    public DcMotorEx outtakeR;
+    // TeleOp Motors and Constructor
+    public DcMotorEx teleOuttakeL;
+    public DcMotorEx teleOuttakeR;
 
     public Shooter (HardwareMap hwMap) {
-//        outtakeL = new MotorEx("outtakeLeft"); // 0
-//        outtakeR = new MotorEx("outtakeRight"); // 2
-        outtakeL = hwMap.get(DcMotorEx.class, "outtakeLeft");
-        outtakeR = hwMap.get(DcMotorEx.class, "outtakeRight");
+        teleOuttakeL = hwMap.get(DcMotorEx.class, "outtakeLeft");
+        teleOuttakeR = hwMap.get(DcMotorEx.class, "outtakeRight");
 
-
-
-        outtakeL.setDirection(DcMotorSimple.Direction.REVERSE);
+        teleOuttakeL.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
-    // Motors for shooter
-//    outtakeL = new MotorEx("outtakeLeft"); // 0
-//    outtakeR = new MotorEx("outtakeRight"); // 2
+    // SHOOTER CALCULATIONS
+    // ---------------------------------
 
-    // Servos for spinny thing (maybe going to be a motor)
-//    private final ServoEx spin1 = new ServoEx("spin1");
-//    private final ServoEx spin2 = new ServoEx("spin2");
+    private static final double launchHeight = 0; // TODO update this with CAD
+    private static final double g = 9.81;
+    private static final double H = 39 - launchHeight; // y distance, cm distance from launch height to a little above hole on goal
+    private static double R; // x distance, get from AprilTag
 
-//    private final ControlSystem outtake_controller = ControlSystem.builder()
-//            .posPid(0.005, 0, 0) //out_controller
-//            .elevatorFF(0) //compensates for gravity
-//            .build();
+    private static final double shootVel = Math.sqrt(H * g + g * Math.sqrt(Math.pow(R, 2) + Math.pow(H, 2)));
 
-//    private static final double launchHeight = 0; // TODO update this with CAD
-//    private static final double g = 9.81;
-//    private static final double H = 39 - launchHeight; // y distance, cm distance from launch height to a little above hole on goal
-//    private static double R; // x distance, get from AprilTag
-//
-//    private static final double shootVel = Math.sqrt(H * g + g * Math.sqrt(Math.pow(R, 2) + Math.pow(H, 2)));
-//
-//    public static double getShootVel() {
-//        return shootVel;
-//    }
+    // AUTONOMOUS COMMANDS - NextFTC
+    // ----------------------------------
 
-//    public enum outtake_vels {
-//
-//        PID_SHOOT(shootVel),
-//        HARDCODED_SHOOT_TRIANGLE(1), // TODO: CALC WITH FIELD
-//        HARDCODED_SHOOT_BACK(1), // TODO: CALC WITH FIELD
-//        IDLE(0);
-//
-//        private final double outtake_vels;
-//
-//        outtake_vels(double pos) {
-//            this.outtake_vels = pos;
-//        }
-//        public double getOuttakeVel() {
-//            return outtake_vels;
-//        }
-//    }
+    private final ControlSystem outtake_controller = ControlSystem.builder()
+            .posPid(0.005, 0, 0) //out_controller
+            .elevatorFF(0) //compensates for gravity
+            .build();
 
-    public void setShooterPower(double lPower, double rPower) {
-        outtakeL.setPower(lPower);
-        outtakeR.setPower(rPower);
+    public enum outtake_vels {
+
+        PID_SHOOT(shootVel),
+        HARDCODED_SHOOT_TRIANGLE(1), // TODO: CALC WITH FIELD
+        HARDCODED_SHOOT_BACK(1), // TODO: CALC WITH FIELD
+        IDLE(0);
+
+        private final double outtake_vels;
+
+        outtake_vels(double pos) {
+            this.outtake_vels = pos;
+        }
+        public double getOuttakeVel() {
+            return outtake_vels;
+        }
     }
 
-//    public Command pidShoot = new RunToVelocity(
-//            outtake_controller,
-//            outtake_vels.PID_SHOOT.getOuttakeVel()
-//    ).requires(this);
-//
-//    public Command triangleShoot = new RunToVelocity(
-//            outtake_controller,
-//            outtake_vels.HARDCODED_SHOOT_TRIANGLE.getOuttakeVel()
-//    ).requires(this);
-//
-//    public Command backShoot = new RunToVelocity(
-//            outtake_controller,
-//            outtake_vels.HARDCODED_SHOOT_BACK.getOuttakeVel()
-//    ).requires(this);
-//
-//    public Command idle = new RunToVelocity(
-//            outtake_controller,
-//            outtake_vels.IDLE.getOuttakeVel()
-//    );
+    public Command pidShoot = new RunToVelocity(
+            outtake_controller,
+            outtake_vels.PID_SHOOT.getOuttakeVel()
+    ).requires(this);
 
-//    public void spinRight() {
-//        spin1.setPosition(spin1.getPosition() + .1);
-//        spin2.setPosition(spin1.getPosition() + .1); // TODO: REVERSE AND MAKE ANALOG METHODS FOR GET POSITION
-//    }
-//
-//    public void spinLeft() {
-//        spin1.setPosition(spin1.getPosition() - .1);
-//        spin2.setPosition(spin1.getPosition() - .1); // TODO: REVERSE AND MAKE ANALOG METHODS FOR GET POSITION
-//    }
+    public Command triangleShoot = new RunToVelocity(
+            outtake_controller,
+            outtake_vels.HARDCODED_SHOOT_TRIANGLE.getOuttakeVel()
+    ).requires(this);
 
-//    @Override
-//    public void periodic() {
-//        outtakeL.setPower(outtake_controller.calculate(outtakeL.getState()));
-//        outtakeR.setPower(outtake_controller.calculate(outtakeR.getState()));
-//
-//    }
+    public Command backShoot = new RunToVelocity(
+            outtake_controller,
+            outtake_vels.HARDCODED_SHOOT_BACK.getOuttakeVel()
+    ).requires(this);
+
+    public Command idle = new RunToVelocity(
+            outtake_controller,
+            outtake_vels.IDLE.getOuttakeVel()
+    );
+
+    @Override
+    public void periodic() {
+        autoOuttakeL.setPower(outtake_controller.calculate(autoOuttakeL.getState()));
+        autoOuttakeR.setPower(outtake_controller.calculate(autoOuttakeR.getState()));
+
+    }
+
+    // TELEOP METHODS
+    // ----------------------------------
+
+    public void setShooterPower(double power) {
+        teleOuttakeL.setPower(power);
+        teleOuttakeR.setPower(power);
+    }
+
+    public static double getShootVel() {
+        return shootVel;
+    }
 }
